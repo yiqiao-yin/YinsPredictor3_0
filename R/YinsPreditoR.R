8c59699a71136fb40b27eae3b763e3aeafd67134
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-03-08 14:43:08 -0500

    Update YinsPreditoR.R

--- R/YinsPreditoR.R
+++ R/YinsPreditoR.R
@@ -306,7 +306,7 @@ yins_predictor <- function(
   # Printing results
   return(list(
     interaction.based.Learning = paste0("Interaction-based Learning: Tomorrow this stock goes up with probability: ", round(interaction.based.probability, 2)),
-    Raw.Buy.Sell.Signal.Table = data.frame(reduced_table[, -1],
+    Raw.Buy.Sell.Signal.Table = data.frame(reduced_table[, -1]),
     Buy.Sell.Signal.Table = knitr::kable(reduced_table[, -1]),
     Statistics = knitr::kable(final_table_stats),
     Comment = paste0(
